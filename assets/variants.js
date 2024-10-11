607a158fe25a8882953fae4ea530086ea688d77d
hompkin <155316709+hompkin@users.noreply.github.com>
2024-10-12 00:32:00 +0800

    support weight

--- assets/variants.js
+++ assets/variants.js
@@ -221,13 +221,13 @@ class VariantSelects extends HTMLElement {
         // if(this.item.find('[data-tab-meta-sku]').length > 0){
         //     this.item.find('[data-tab-meta-sku] .value-text').text(this.currentVariant.sku);
         // }
-        const weightDimensionsTab = document.getElementById('tab-specification-amp-dimensions-mobile');
+        const weightDimensionsTab = document.getElementById('tab-specification-amp-dimensions');
         if (weightDimensionsTab) {
             fetch(`${this.dataset.url}?variant=${this.currentVariant.id}&section_id=${this.dataset.section}`)
                 .then((response) => response.text())
                 .then((responseText) => {
                     const html = new DOMParser().parseFromString(responseText, 'text/html');
-                    const sourceTab = html.getElementById('tab-weight-dimensions');
+                    const sourceTab = html.getElementById('tab-specification-amp-dimensions');
                     if (sourceTab) {
                         weightDimensionsTab.innerHTML = sourceTab.innerHTML;
                     }
